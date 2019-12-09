f6513b08937646ac88108075c5a737f8a4e16c18
PilarPinto <piapintoch@unal.edu.co>
2019-12-09 22:45:01 +0000

    [Fix]- Addin a conditional of list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,6 +9,9 @@ int check_cycle(listint_t *list)
 	listint_t *slow_p = list;
 	listint_t *fast_p = list;
 
+	if (list == NULL)
+		return (0);
+
 	fast_p = fast_p->next;
 
 	while (slow_p != NULL && fast_p != NULL && fast_p->next != NULL)
