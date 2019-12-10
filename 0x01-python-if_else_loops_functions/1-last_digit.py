5a4d69ff173959682b5116247d9db4c69ee8275f
PilarPinto <piapintoch@unal.edu.co>
2019-12-10 16:04:39 +0000

    [Feature]-Give is thhe last number is > < = to 5

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_n = number % 10
+print('Last digit of {:d} is {:d} '.format(number,last_n), end = '')
+if last_n > 5:
+    print('and is greater than 5')
+elif last_n < 6 and last_n != 0:
+    print('and is less than 6 and not 0')
+else:
+    print('and is 0')
