d14c249614f3be7b1af737b252203edd850fe3fb
PilarPinto <piapintoch@unal.edu.co>
2019-12-13 00:57:32 +0000

    [Fix]: Addin name set main

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-from sys import argv
-import calculator_1 as opr
-if not (len(argv) == 4):
-    print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-    exit(1)
-
-nb1 = int(argv[1])
-nb2 = int(argv[3])
-symb = argv[2]
-
-ops = {'+': opr.add, '-': opr.sub, '*': opr.mul, '/': opr.div}
-for ind in ops:
-    if symb == ind:
-        print('{} {} {} = {}'.format(nb1, symb, nb2, ops[ind](nb1, nb2)))
-        exit(0)
-    else:
-        print('Unknown operator. Available operators: +, -, * and /')
+if __name__ == '__main__':
+    from sys import argv
+    import calculator_1 as opr
+    if not (len(argv) == 4):
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
+
+    nb1 = int(argv[1])
+    nb2 = int(argv[3])
+    symb = argv[2]
+
+    ops = {'+': opr.add, '-': opr.sub, '*': opr.mul, '/': opr.div}
+    for ind in ops:
+        if symb == ind:
+            print('{} {} {} = {}'.format(nb1, symb, nb2, ops[ind](nb1, nb2)))
+            exit(0)
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
