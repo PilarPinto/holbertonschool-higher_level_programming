88abd97f1d0785d2a5b1b91a018cfb1ca23bea9d
PilarPinto <piapintoch@unal.edu.co>
2019-12-17 21:06:54 +0000

    [Fix]-Remove iterator

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    list = my_list.copy()
-    if idx < 0:
-        return list
-    if idx > len(my_list):
-        return list
-    for number in range(0, len(list)):
-        if (number == idx):
-            list[number] = element
-            return list
+    c_list = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return c_list
+    c_list[idx] = element
+    return c_list
