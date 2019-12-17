5c8f054392fe7e236482a443ce6598602aec7b30
PilarPinto <piapintoch@unal.edu.co>
2019-12-17 20:24:38 +0000

    [Fix]-Adding conditionals and delete iterator

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    elif idx > len(my_list):
         return my_list
-    for number in range(0, len(my_list)):
-        if (number == idx):
-            my_list[number] = element
-            return my_list
+    else:
+        my_list[idx] = element
+        return my_list
