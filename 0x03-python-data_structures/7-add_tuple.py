504ca9eefd05dcfa323f0397bd32494d4f5a15cc
PilarPinto <piapintoch@unal.edu.co>
2019-12-17 23:13:13 +0000

    [Feature]-Adding two tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    list_tup = []
+    for indx in range(0, len(tuple_a)):
+        try:
+            valuea = tuple_a[indx]
+        except IndexError:
+            valuea = 0
+        try:
+            valueb = tuple_b[indx]
+        except IndexError:
+            valueb = 0
+        list_tup.append(valuea + valueb)
+    return tuple(list_tup)
+
