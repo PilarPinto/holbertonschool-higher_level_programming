1dceee06bd2312a8b1936d22f5aefcaa1d0aa90e
PilarPinto <piapintoch@unal.edu.co>
2019-12-18 00:32:33 +0000

    [Feature]-Instance check

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    bool_list = []
-    for ind in my_list:
-        if (my_list[ind] % 2 == 0):
-            bool_list.append(True)
-        else:
-            bool_list.append(False)
-    return bool_list
+    if isinstance(my_list, list):
+        bool_list = []
+        for ind in my_list:
+            if (my_list[ind] % 2 == 0):
+                bool_list.append(True)
+            else:
+                bool_list.append(False)
+        return bool_list
