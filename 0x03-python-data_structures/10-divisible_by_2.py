7ba590a9f94c2e56e18290e2179cbcb4e6a98b59
PilarPinto <piapintoch@unal.edu.co>
2019-12-18 00:36:08 +0000

    [Fix]-More pythonic code

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if isinstance(my_list, list):
-        bool_list = []
-        for ind in my_list:
-            if (my_list[ind] % 2 == 0):
-                bool_list.append(True)
-            else:
-                bool_list.append(False)
-        return bool_list
+    return list(map(lambda i: i % 2 == 0, my_list))
