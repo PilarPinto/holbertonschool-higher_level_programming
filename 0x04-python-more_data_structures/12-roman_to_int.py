820e95102fb72ce0fab513c82e78a166fdd84a00
PilarPinto <piapintoch@unal.edu.co>
2019-12-20 02:34:29 +0000

    [Feature]- Using dictionary to create a roman number equivelence

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) is not str or len(roman_string) is 0:
+        return 0
     letters = roman_string
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     ind = 0
