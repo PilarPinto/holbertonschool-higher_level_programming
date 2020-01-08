fc2f09c05367774c44583c823a6f20bac291a671
PilarPinto <piapintoch@unal.edu.co>
2020-01-08 19:48:53 +0000

    [Fix]-Correct cythonic error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0;
+    count = 0
     try:
         for ind in range(x):
             print("{}".format(my_list[ind]), end='')
