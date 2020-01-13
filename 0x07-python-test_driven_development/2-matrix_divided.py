d5e4efbdc7f077e752e978dae1cc5c360abff298
PilarPinto <piapintoch@unal.edu.co>
2020-01-13 06:09:22 +0000

    [Feature]- Correct output before testing

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' Function to divides all matrix's elements'''
+
+
+def matrix_divided(matrix, div):
+    '''Function two divide each element by a number'''
+    if type(matrix) is not list:
+        raise TypeError('matrix must be a matrix (list of lists) \
+        of integers/floats')
+
+    for lst in matrix:
+        for number in lst:
+            if not(isinstance(number, int) or isinstance(number, float)):
+                raise TypeError('matrix must be a matrix \
+                (list of lists) of integers/floats')
+
+    if not all(map(lambda lst: len(matrix[0]) == len(lst), matrix)):
+        raise TypeError('Each row of the matrix must have the same \
+        size')
+
+    if not(type(div) is int or type(div) is float):
+        raise TypeError('div must be a number')
+
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    return ([list(map(lambda nm: round(nm / div, 2), lst)) for lst in matrix])
