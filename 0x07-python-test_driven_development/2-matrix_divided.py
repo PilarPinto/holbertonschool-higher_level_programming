48e40d20b536717906d079b098a7404daf8e15e9
PilarPinto <piapintoch@unal.edu.co>
2020-01-14 02:34:51 +0000

    [Fix]: error of reference a to b

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,19 +4,19 @@
 
 def matrix_divided(matrix, div):
     '''Function two divide each element by a number'''
+    long_error = 'matrix must be a matrix (list of lists) of integers/floats'
     if type(matrix) is not list:
-        raise TypeError('matrix must be a matrix (list of lists) \
-        of integers/floats')
+        raise TypeError(
+            'matrix must be a matrix (list of lists) of integers/floats')
 
     for lst in matrix:
         for number in lst:
             if not(isinstance(number, int) or isinstance(number, float)):
-                raise TypeError('matrix must be a matrix \
-                (list of lists) of integers/floats')
+                raise TypeError(long_error)
 
     if not all(map(lambda lst: len(matrix[0]) == len(lst), matrix)):
-        raise TypeError('Each row of the matrix must have the same \
-        size')
+        raise TypeError(
+            'Each row of the matrix must have the same size')
 
     if not(type(div) is int or type(div) is float):
         raise TypeError('div must be a number')
