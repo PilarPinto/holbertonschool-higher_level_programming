db58d352c3e3af34329bdfa2eb7084736621be6e
PilarPinto <piapintoch@unal.edu.co>
2020-01-14 02:41:50 +0000

    [Fix]: Adding more comments

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-'''Print a square of hastags'''
+'''
+Print a square of hastags
+the function receives a number
+that indicate the size of the side
+'''
 
 
 def print_square(size):
-    '''Function to print a square of hastags'''
+    '''
+    Function to print a square of hastags
+    '''
     if not (isinstance(size, int)):
         raise TypeError('size must be an integer')
     if size < 0:
