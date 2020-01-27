bc21ca9cface50c8db19ec8289511a0d9b1af180
PilarPinto <piapintoch@unal.edu.co>
2020-01-27 19:03:07 +0000

    [Testing]-Comment

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,13 +8,14 @@ from models.base import Base
 
 class Rectangle(Base):
     '''Class rectangle'''
+
     def __init__(self, width, height, x=0, y=0, id=None):
         '''Constructor'''
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
@@ -42,8 +43,7 @@ class Rectangle(Base):
             raise TypeError('height must be an integer')
         elif valueh <= 0:
             raise ValueError('height must be > 0')
-        else:
-            self.__height = valueh
+        self.__height = valueh
 
     @property
     def x(self):
@@ -57,8 +57,7 @@ class Rectangle(Base):
             raise TypeError('x must be an integer')
         elif value < 0:
             raise ValueError('x must be >= 0')
-        else:
-            self.__x = value
+        self.__x = value
 
     @property
     def y(self):
@@ -72,8 +71,7 @@ class Rectangle(Base):
             raise TypeError('y must be an integer')
         elif value < 0:
             raise ValueError('y must be >= 0')
-        else:
-            self.__y = value
+        self.__y = value
 
     def area(self):
         '''Area function'''
