ebead483d93175ad7eafc98883c5954c9859550c
PilarPinto <piapintoch@unal.edu.co>
2020-03-28 20:47:19 +0000

    [Fix]-Addign the pattern to capitalized letter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,8 @@ def mysqlconnect():
     )
 
     cursor = db_connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE name REGEXP '^N'")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'"
+                   "COLLATE Latin1_General_CS;")
     states = cursor.fetchall()
 
     for state in states:
