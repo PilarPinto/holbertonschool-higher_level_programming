62a588070a503fbb31f6c3737eb15343162ac45a
PilarPinto <piapintoch@unal.edu.co>
2020-04-15 22:05:22 +0000

    [Fix]:remove unused import

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 '''The function to fetches https://intranet.hbtn.io/status'''
 import requests
-import sys
 
 if __name__ == "__main__":
-    var = requests.get('https://intranet.hbtn.io/status')
+    va = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type: {}'.format(type(str(var))))
-    print('\t- content: {}'.format(var.text))
-    var.close()
+    print('\t- type: {}'.format(type(str(va))))
+    print('\t- content: {}'.format(va.text))
+    va.close()
