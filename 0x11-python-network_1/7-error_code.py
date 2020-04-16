0d3c3a81f47d750b4c522847cfb87fa30e98ef81
PilarPinto <piapintoch@unal.edu.co>
2020-04-16 06:46:52 +0000

    [Fix]:Typo error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,8 +6,9 @@ and displays the body of the response.
 import requests
 import sys
 
+
 if __name__ == "__main__":
-    var = requests.get(sys.arv[1])
+    var = requests.get(sys.argv[1])
     if (int(var.status_code) >= 400):
         print("Error code: {}".format(var.status_code))
     else:
